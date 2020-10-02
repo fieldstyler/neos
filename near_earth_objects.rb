19406a3eb181de99e0e7222e35c434284524edc9
fieldstyler <tdfields93@gmail.com>
2020-10-02 13:05:01 -0600

    Refactor to use helper methods

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -7,31 +7,51 @@ Figaro.load
 
 class NearEarthObjects
   def self.find_neos_by_date(date)
-    conn = Faraday.new(
+    @date = date
+    @conn = Faraday.new(
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
-    asteroids_list_data = conn.get('/neo/rest/v1/feed')
+    self.asteroids_list_data
+    self.parsed_asteroids_data
+    self.largest_asteroid_diameter
+    self.total_number_of_asteroids
+    self.formatted_asteroid_data
+  end
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  def self.asteroids_list_data
+    asteroids_list_data = @conn.get('/neo/rest/v1/feed')
+  end
 
-    largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
-      astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+  def self.parsed_asteroids_data
+    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{@date}"]
+  end
+
+  def self.largest_asteroid_diameter
+    self.parsed_asteroids_data
+    largest_asteroid_diameter = parsed_asteroids_data.map do |asteroid|
+      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
+  end
 
-    total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
+  def self.total_number_of_asteroids
+    total_number_of_asteroids = parsed_asteroids_data.count
+  end
+
+  def self.formatted_asteroid_data
+    self.largest_asteroid_diameter
+    self.total_number_of_asteroids
+    formatted_asteroid_data = parsed_asteroids_data.map do |asteroid|
       {
-        name: astroid[:name],
-        diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+        name: asteroid[:name],
+        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
-
     {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
+      asteroid_list: formatted_asteroid_data,
+      biggest_asteroid: largest_asteroid_diameter,
+      total_number_of_asteroids: total_number_of_asteroids
     }
   end
 end
